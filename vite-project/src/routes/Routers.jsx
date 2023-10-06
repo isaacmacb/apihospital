3d4df7f760a81759c17f50a2af408fb1f19c75fb
isaacmacb <liberdadebessa@gmail.com>
2023-10-06 20:44:48 -0300

    corrigindo erros

--- vite-project/src/routes/Routers.jsx
+++ vite-project/src/routes/Routers.jsx
@@ -1,31 +1,28 @@
-import React from 'react'
-import Home from '../pages/Home'
-import Services from '../pages/Services'
-import Login  from '../pages/Login'
-import Signup from '../pages/Signup'
-import Contact from '../pages/Contact'
-import Doctors from '../pages/Doctors/Doctors'
-import DoctorDetails from '../pages/Doctors/DoctorDetails'
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Home from '../pages/Home';
+import Services from '../pages/Services';
+import { Login } from '../pages/Login';
+import { Signup } from '../pages/Signup'; // Importe o Signup corretamente
+import Contact from '../pages/Contact';
+import Doctors from '../pages/Doctors/Doctors';
+import DoctorDetails from '../pages/Doctors/DoctorDetails';
 
-import {Routes, Route} from 'react-router-dom'
-
-
+import { Routes, Route } from 'react-router-dom';
 
 const Routers = () => {
-  return <Routes>
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/home' element={<Home />} />
+      <Route path='/doctors' element={<Doctors />} />
+      <Route path='/doctors/:id' element={<DoctorDetails />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Signup />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='/services' element={<Services />} />
+    </Routes>
+  );
+};
 
-    <Route path='/' element = { <Home />}/>
-    <Route path='/home' element = { <Home />}/>
-    <Route path='/doctors' element = { <Doctors />}/>
-    <Route path='/doctors/:id' element = { <DoctorDetails />}/>
-    <Route path='/login' element = { <Login />}/>
-    <Route path='/register' element = { <Signup />}/>
-    <Route path='/contact' element = { <Contact />}/>
-    <Route path='/services' element = { <Services />}/>
-
-
-
-  </Routes>
-}
-
-export default Routers
+export default Routers;
--- vite-project/tailwind.config.js
+++ vite-project/tailwind.config.js
@@ -1,19 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "*./index.html",
-    "*./src/**/*.{js, ts, jsx, tsx}",
+    "./index.html", // Remova o asterisco (*)
+    "./src/**/*.{js,ts,jsx,tsx}", // Remova o asterisco (*)
   ],
   theme: {
-    extend: {  
-    primaryColor: "#0067FF",
-    yellowColor: "#FEB60D",
-    purpleColor: "#9771FF",
-    irisBlueColor: "#01B5C5",
-    headingColor: "#181A1E",
-    textColor: "#4E545F",},
+    extend: {
+      primaryColor: "#0067FF",
+      yellowColor: "#FEB60D",
+      purpleColor: "#9771FF",
+      irisBlueColor: "#01B5C5",
+      headingColor: "#181A1E",
+      textColor: "#4E545F",
+    },
   },
   plugins: [],
 }
-
-
