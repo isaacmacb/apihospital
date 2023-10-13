503fc7dc33dfda18716b45b3d678085a1369813a
isaacmacb <liberdadebessa@gmail.com>
2023-10-13 04:02:57 -0300

    consertando alguns bugs

--- vite-project/src/components/Header/Header.jsx
+++ vite-project/src/components/Header/Header.jsx
@@ -1,8 +1,54 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import {userEffecet, useRef } from 'react'
+import { useEffect } from 'react'; // Corrigi um erro de digitação na importação
+import logo from '../../assets/images/logo.png';
+import { NavLink } from 'react-router-dom';
+
+const navLinks = [
+  {
+    path: '/home',
+    display: 'Home',
+  },
+  {
+    path: '/doctors',
+    display: 'Find a Doctor',
+  },
+  {
+    path: '/services',
+    display: 'Services',
+  },
+  {
+    path: '/contact',
+    display: 'Contact',
+  },
+];
 
 const Header = () => {
-    return <div>Header</div>
-}
+  return (
+    <header className='header flex items-center'>
+      <div className='container'>
+        <div className='flex items-center justify-between'>
+          {/*============logo===========*/}
+          <div>
+            <img src={logo} width={"100px"} alt="" />
+          </div>
+
+          {/*============menu===========*/}
+          <div className='navigation'>
+            <ul className="menu flex items-center gap-[2.7rem]">
+              {navLinks.map((link, index) => (
+                <li key={index}>
+                  <NavLink to={link.path} activeClassName="active">
+                    {link.display}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
