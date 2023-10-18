b45bd9855798865053deb0e70e9929f99365164d
isaacmacb <liberdadebessa@gmail.com>
2023-10-18 11:57:15 -0300

    inserindo as fotos dos doutores

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-
+import heroImg01 from '../assets/images/hero-img.png'
 const Home = () => {
   return (
     <>
       {/* ===========hero section=========== */}
-      <section className="hero_section pt-[60px] 2xl:h-[800px]">
+      <section className="hero__section pt-[60px] 2xl:h-[800px]">
         <div className='container'>
           <div className='flex flex-col lg:flex-row gap-[90px] items-center justify-between'>
             {/* ===========hero content=========== */}
@@ -21,9 +21,43 @@ const Home = () => {
               </div>
                           {/* ===========hero counter=========== */}
                 <div className="mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px]">
+                    <div>
+                          <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                              30+
+                          </h2>
+                          <span className="w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]"></span>
+                          <p className="text__para">Years of Experience</p>
+                    </div>
+
+                    <div>
+                          <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                              15+
+                          </h2>
+                          <span className="w-[100px] h-2 bg-purpleColor rounded-full block mt-[-14px]"></span>
+                          <p className="text__para">Clinic Location</p>
+                    </div>
+
+                    <div>
+                          <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor">
+                              100%
+                          </h2>
+                          <span className="w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]"></span>
+                          <p className="text__para">Patient Satisfaction</p>
+                    </div>
 
                 </div>
             </div>
+                                        {/* ===========hero content=========== */}
+
+
+              <div className='flex gap-[30px] justify-end'>
+                  <div>
+                    <img className="w-full" src={heroImg01} alt="" />
+                  </div>
+                  <div>
+
+                  </div>
+              </div>
 
           </div>
         </div>
