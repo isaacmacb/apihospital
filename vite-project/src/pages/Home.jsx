0e00e6785100305241b5cd5e2bf868261da2506c
isaacmacb <liberdadebessa@gmail.com>
2023-10-23 00:36:58 -0300

    services doctors

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -9,10 +9,15 @@ import icon02 from '../assets/images/icon02.png'
 import icon03 from '../assets/images/icon03.png'
 import featureImg from '../assets/images/feature-img.png'
 import videoicon from '../assets/images/video-icon.png'
+import avatar from '../assets/images/avatar-icon.png'
+
+
+
 import { Link } from 'react-router-dom';
 import { BsArrowRight } from 'react-icons/bs'
 import About from '../components/About/About';
 import ServiceLister from '../components/Service/ServiceLister';
+import DoctorList from '../components/Doctors/DoctorList';
 const Home = () => {
   return (
     <>
@@ -202,11 +207,12 @@ const Home = () => {
             </div>
             {/* ===========feature img=========== */}
             <div className='relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0'>
-              <img src={featureImg} alt="" />
+              <img src={featureImg} className='w-3/4' alt="" />
 
-              <div className='w-[150] lg:w-[248px] bg-whiTe absolute bottom-[50px] left-0 md:bottom-[100px]
+              <div className='w-[150] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px]
               md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:px-4 lg:pb-[26px] rounded-[10px]'>
-                <div className='flex items-center gap-[6px] lg:gap-3'>
+                <div className='flex items-center justify-between'>
+<div className='flex items-center gap-[6px] lg:gap-3'>
                   <p className='text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]'>
                     Tue,24
                   </p>
@@ -219,6 +225,16 @@ const Home = () => {
                     </span>
                   </div>
                 </div>
+                </div>
+                <div className='w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 
+                text-irisBlueColor font-[500] mt-2 lg:mt-4
+                rounded-full'>
+                    Consultation
+                </div>
+                <div className='flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px]'>
+                    <img src={avatar} />
+                    <h4 className='text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-headingColor'>Wayne Collins</h4>
+                </div>  
               </div>
             </div>
           </div>
@@ -227,6 +243,22 @@ const Home = () => {
 
       {/* ===========feature section end=========== */}
 
+
+      {/* ===========our great doctors=========== */}
+      <section>
+        <div className='container'>
+          <div className='xl:w-[470] mx-auto'>
+          <h2 className='heading text-center'>Our Great doctors</h2>
+            <p className='text__para text-center'>
+              World-class care for everyone. Our health system offers unmatched
+            </p>
+          </div>
+
+        <DoctorList/>
+
+        </div>
+      </section>
+              {/* ===========our great doctors=========== */}
 
     </>
   );
