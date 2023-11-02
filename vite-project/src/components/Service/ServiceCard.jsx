f5f8181302ac7f6ee5986aba500d86907981f82e
isaacmacb <liberdadebessa@gmail.com>
2023-11-02 13:46:40 -0300

    finalizando algumas partes

--- vite-project/src/components/Service/ServiceCard.jsx
+++ vite-project/src/components/Service/ServiceCard.jsx
@@ -1,10 +1,12 @@
-import {Link} from 'react-router-dom'
-import { BsArrowRight } from 'react-icons/bs'
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { BsArrowRight } from 'react-icons/bs';
 
-const ServiceCard = ({item, index}) => {
-    const {name, desc, bgColor, textColor} = item
+const ServiceCard = ({ item }) => {
+    const { name, desc, bgColor, textColor } = item;
 
-    return <div className="py-[30] px-3 lg:px-5">
+    return (
+        <div className="py-[30] px-3 lg:px-5">
             <h2 className="text-[26px] leading-9 text-headingColor font-[700]">
                 {name}
             </h2>
@@ -13,14 +15,23 @@ const ServiceCard = ({item, index}) => {
             </h2>
 
             <div className="flex items-center justify-between mt-[30px]">
-            <Link to="/doctors" className="w-[44px] rounded-full border border-solid border-[#181E1E]  flex items-center justify-center group hover:bg-primaryColor hover:border-non" >
-              <BsArrowRight className='group-hover:text-white w-6 h-5'/>
-            </Link>
-            <span className='w-[44px] h-[44px] flex items-center justify-centr text-[18px] leading-[30px] font-[600]' style={{background: `${bgColor}`, color: `${textColor}`, borderRadius: "6px 0 0 6px"}}>
-            
-            </span>
+                <Link to="/doctors" className="w-[44px] rounded-full border border-solid border-[#181E1E]  flex items-center justify-center group hover:bg-primaryColor hover:border-non">
+                    <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+                </Link>
+                <span className='w-[44px] h-[44px] flex items-center justify-center text-[18px] leading-[30px] font-[600]' style={{ background: `${bgColor}`, color: `${textColor}`, borderRadius: "6px 0 0 6px" }}>
+                </span>
             </div>
-    </div>
-}
+        </div>
+    );
+};
 
-export default ServiceCard
+ServiceCard.propTypes = {
+    item: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        desc: PropTypes.string.isRequired,
+        bgColor: PropTypes.string.isRequired,
+        textColor: PropTypes.string.isRequired,
+    }).isRequired,
+};
+
+export default ServiceCard;
--- vite-project/src/pages/Services.jsx
+++ vite-project/src/pages/Services.jsx
@@ -1,10 +1,28 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
+import { services } from "../assets/data/service";
+import useTitle from "../Hooks/useTitle.js";
+import ServicesCard from "../components/Service/ServiceCard";
 
-const Service = () => {
+const Services = () => {
+  useTitle("Services");
+
   return (
-    <div>Service</div>
-  )
-}
+    <>
+      <section className="bg-[#fff9ea]">
+        <div className="container text-center">
+          <h2 className="heading">Services</h2>
+        </div>
+      </section>
+      <section>
+        <div className="container">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
+            {services.map((item, index) => (
+              <ServicesCard item={item} key={index} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Service
+export default Services;
